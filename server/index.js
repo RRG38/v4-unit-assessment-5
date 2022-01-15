38b7bf1e4b0e1f5b0dc154b3fdd0ad1b2b713f30
Ryan Garren <rryangarren@gmail.com>
2022-01-15 16:32:00 -0700

    started over, db and server running

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const massive = require('massive')
 const express = require('express'),
       userCtrl = require('./controllers/user'),
       postCtrl = require('./controllers/posts')
@@ -6,18 +7,30 @@ const express = require('express'),
 
 const app = express();
 
+const { CONNECTION_STRING, SERVER_PORT } = process.env
+
 app.use(express.json());
 
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false }
+}).then(db => {
+  app.set('db', db);
+  console.log('DB connection established!');
+}).catch(err => {
+  console.log(`Error connecting to DB: ${err}`);
+})
+
 //Auth Endpoints
-app.post('/api/auth/register', userCtrl.register);
-app.post('/api/auth/login', userCtrl.login);
-app.get('/api/auth/me', userCtrl.getUser);
-app.post('/api/auth/logout', userCtrl.logout);
+// app.post('/api/auth/register', userCtrl.register);
+// app.post('/api/auth/login', userCtrl.login);
+// app.get('/api/auth/me', userCtrl.getUser);
+// app.post('/api/auth/logout', userCtrl.logout);
 
-//Post Endpoints
-app.get('/api/posts', postCtrl.readPosts);
-app.post('/api/post', postCtrl.createPost);
-app.get('/api/post/:id', postCtrl.readPost);
-app.delete('/api/post/:id', postCtrl.deletePost)
+// //Post Endpoints
+// app.get('/api/posts', postCtrl.readPosts);
+// app.post('/api/post', postCtrl.createPost);
+// app.get('/api/post/:id', postCtrl.readPost);
+// app.delete('/api/post/:id', postCtrl.deletePost)
 
-app.listen(4000, _ => console.log(`running on ${4000}`));
+app.listen(SERVER_PORT, _ => console.log(`running on ${SERVER_PORT}`));
